daa32caea0d0a51a26b81489fb0bdc24a6108baf
Szymon Zimnowoda <szymek156@o2.pl>
2021-12-16 23:48:19 +0100

    added example to run quad

--- src/main.rs
+++ src/main.rs
@@ -25,33 +25,37 @@ mod quadruple;
 mod soft_float;
 
 fn main() {
-    let mut fractal = Fractal::<f64>::default()
+    let mut fractal =
+            // Fractal::<Quad>::default()
+            // .with(Box::new(Mandelbrot(PhantomData)))
+            // .with_poi(PoI {
+            //     // Limit of Quad, yay!
+            //     origin_x: Quad {
+            //         lo: 0.000000000000000010150844351198857,
+            //         hi: -1.275160031112145,
+            //     },
+            //     origin_y: Quad {
+            //         lo: 0.000000000000000006705298387715869,
+            //         hi: -0.19410769865119987,
+            //     },
+            //     pinhole_size: Quad {
+            //         lo: 0.00000000000000000000000000000000000000000000002084738436862751,
+            //         hi: 0.0000000000000000000000000000004272805497045327,
+            //     },
+            //     limit: 1400,
+            // })
+        Fractal::<f64>::default()
         .with(Box::new(MandelbrotAvx2))
         .with_poi(PoI {
             // Template type deduction!
-            origin_x: -1.275160031112145,
-            origin_y: -0.19410769865119987,
-            pinhole_size: 0.000000000000000038614262509059454,
-            limit: 1200,
-            // Limit of Quad, yay!
-            // origin_x: Quad {
-            //     lo: 0.000000000000000010150844351198857,
-            //     hi: -1.275160031112145,
-            // },
-            // origin_y: Quad {
-            //     lo: 0.000000000000000006705298387715869,
-            //     hi: -0.19410769865119987,
-            // },
-            // pinhole_size: Quad {
-            //     lo: 0.00000000000000000000000000000000000000000000002084738436862751,
-            //     hi: 0.0000000000000000000000000000004272805497045327,
-            // },
-            // limit: 1400,
-
-            // origin_x: -0.7436438870371587,
-            // origin_y: 0.13182590420531198,
-            // pinhole_size: 0.0000000000004892965009859402,
-            // limit: 3800,
+            // origin_x: -1.275160031112145,
+            // origin_y: -0.19410769865119987,
+            // pinhole_size: 0.000000000000000038614262509059454,
+            // limit: 1200,
+            origin_x: -0.7436438870371587,
+            origin_y: 0.13182590420531198,
+            pinhole_size: 0.0000000000004892965009859402,
+            limit: 3800,
         })
         .run_on(ExecutorKind::Rayon);
 
